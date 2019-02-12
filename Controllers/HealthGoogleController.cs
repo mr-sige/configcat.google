d23a63aec6dc316026b1f329a127eca021a6cbb4
sige <sige@sige.hu>
2019-02-12 16:41:59 +0100

    hello

---
+++ Controllers/HealthGoogleController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Google.Apis.Dialogflow.v2.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace configcat.assistant.api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HealthGoogleController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Post([FromBody] GoogleCloudDialogflowV2WebhookRequest request)
+        {
+            var response = new GoogleCloudDialogflowV2WebhookResponse();
+
+            response.FulfillmentText = "ConfigCat rules!";
+            return new OkObjectResult(response);
+        }
+    }
+}
